b8f859f62fef3744c59e3b7cef577da2470bddc0
Tom MacWright <tom@macwright.org>
2014-11-26 18:25:28 -0500

    Update build

--- index.js
+++ index.js
@@ -87,16 +87,16 @@ var Result = React.createClass({
     /* jshint ignore:start */
     return <div className='keyline-bottom pad0 col12 clearfix'>
       <div className='col6'>
-        <h2>
+        <h3>
           <a href={osmURL}>
             {this.props.res.tags.name}
           </a>
-        </h2>
+        </h3>
       </div>
       <div className='col6'>
         {(!this.state.update && this.props.res.tags['cost:coffee']) ?
           (<div className='pad0y'>
-             <strong>{this.props.res.tags['cost:coffee']}</strong>
+             <strong>${this.props.res.tags['cost:coffee']}</strong>
              <a
               onClick={this.setUpdate}
               href='#'
@@ -167,16 +167,22 @@ var Page = React.createClass({
       <div className='col6 margin3'>
         <div className='pad1'>
           <h1 className='center'>COFFEE DEX</h1>
-          <div className='pad1 center'>How much does a cup of house coffee for here cost, everywhere?</div>
+          <div className='pad1 center'>
+             <p>How much does a cup of house coffee for here cost, everywhere?</p>
+              <p>load coffee shops within 0.004 degrees of you, and when you edit and save a price, it's added to openstreetmap.</p>
+              <p>prices should be for the cheapest for-here coffee on the menu.</p>
+              </div>
         </div>
         {!this.state.user && <a href='#' className='fill-green dark center pad1 col12 icon user' onClick={this.authenticate}>AUTHENTICATE</a>}
         {!this.state.location && <a href='#' className='fill-blue center dark pad1 col12 icon compass' onClick={this.locate}>LOCATE</a>}
         {this.state.location && <div className='col12 center pad1 fill-grey code'>
-          {this.state.location.coords.latitude.toFixed(3)},
-          {this.state.location.coords.longitude.toFixed(3)}
+          {this.state.location.coords.latitude.toFixed(3)}, {this.state.location.coords.longitude.toFixed(3)}
           </div>}
         {this.state.loading && <h1 className='pad1 center'>LOADING</h1>}
         {this.state.results.map(res => <Result key={res.id} res={res} />)}
+        <div className='pad1 keyline-top center'>
+            <a href='https://github.com/tmcw/coffeedex'>github.com/tmcw/coffeedex</a>
+        </div>
       </div>
     );
     /* jshint ignore:end */
